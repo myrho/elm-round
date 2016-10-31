6fe4404d7d6b0db8eaf2dcd853caebf5e3b54137
Matthias <git@myrho.net>
2016-10-31 15:44:11 +0100

    add docs, btw fix toDecimal, add another test for toDecimal

--- src/Round.elm
+++ src/Round.elm
@@ -1,19 +1,84 @@
 module Round exposing 
-  ( roundNum, floorNum, ceilingNum
-  , floorNumCom, ceilingNumCom
-  , toDecimal
-  , round, ceiling, floor, truncate
+  ( round, ceiling, floor
   , roundCom, ceilingCom, floorCom
+  , roundNum, floorNum, ceilingNum
+  , roundNumCom, floorNumCom, ceilingNumCom
+  , toDecimal, truncate
   )
+
+{-| This library empowers you to convert a `Float` to a `String` with ultimate control how many digits after the decimal point are shown and how the remaining digits are rounded. You can round, round up and round down the mathematical or the commerical way. For example:
+
+    x = 3.141592653589793
+
+    round 2 x -- "3.14"
+    round 4 x -- "3.1416"
+    
+    ceiling 2 x -- "3.15"
+    floor 4 x -- "3.1415"
+
+[Commercial rounding](https://en.wikipedia.org/wiki/Rounding#Round_half_away_from_zero) means that negative and positive numbers are treated symmetrically. It affects numbers whose last digit equals 5. For example:
+
+    x = -0.5
+
+    round 0 x -- "0"
+    roundCom 0 x -- "-1"
+
+    floor 0 x -- "-1"
+    floorCom 0 x -- "0"
+
+    ceiling 0 x -- "0"
+    ceilingCom 0 x -- "-1"
+
+Have a look at the tests for more examples!
+
+Under the hood the `Float` is 
+
+  # converted to a string
+  # normalized (if it is in scientific notation, eg `1.234e-23`)
+  # splitted at the comma.
+  # The the part after comma is truncated to the number of desired digits + 1
+  # inserting a comma before the last digit,
+  # turning this into a `Float` again,
+  # apply a rounding function on it,
+  # attach it the part before comma again.
+  # By the way handles cases with already rounded numbers, zero and the sign.
+
+Why aren't we just doing `x * 1000 |> round |> toFloat |> (flip (/)) 1000` in order to round to 3 digits after comma? Because due to floating point arithmetic it might happen that it outputs someting like `3.1416000000001`, although we just wanted `3.1416`. Ugly.
+
+# Round to String
+@docs round, ceiling, floor, roundCom, ceilingCom, floorCom
+
+# Round to Float
+@docs roundNum, ceilingNum, floorNum, roundNumCom, ceilingNumCom, floorNumCom
+
+# Utility functions
+@docs toDecimal, truncate
+
+-}
 
 import String
 
+{-| Like Elm's basic `truncate` but works on the full length of a float's 64 bits. So it's more precise.
+
+    x = 9007199254740.99
+
+    Basics.truncate x -- outputs 652835028 (which is not correct)
+    Round.truncate x -- output 9007199254740 (which is)
+-}
 truncate : Float -> Int
 truncate n =
   if n < 0
     then Basics.ceiling n
     else Basics.floor n
 
+{-| Transforms a `Float` in scientific notation into its decimal representation as a `String`.
+
+    x = 1e30
+    toDecimal x -- outputs "1000000000000000000000000000000"
+
+    x = 1.2345e-30
+    toDecimal x -- outputs "0.0000000000000000000000000000012345"
+-}
 toDecimal : Float -> String
 toDecimal fl =
   case String.split "e"
@@ -21,7 +86,10 @@ toDecimal fl =
     num :: exp :: _ ->
       let
         e = 
-          String.toInt exp
+          ( if String.startsWith "+" exp
+              then String.dropLeft 1 exp
+              else exp
+          ) |> String.toInt
             |> Result.toMaybe
             |> Maybe.withDefault 0
         (sign, before,after) =
@@ -86,8 +154,6 @@ toDecimal fl =
     _ ->
       ""
 
-
-        
 splitComma : String -> (String,String)
 splitComma str =
   case String.split "." str of
@@ -158,19 +224,45 @@ roundFun functor s fl =
           ++ "."
           ++ g
 
+{-| Turns a `Float` into a `String` and rounds it to the given number of digits after decimal point.
+
+    x = 3.141592653589793
+
+    round 2 x -- "3.14"
+    round 4 x -- "3.1416"
+-}
 round : Int -> Float -> String
 round =
   roundFun Basics.round
 
+{-| Turns a `Float` into a `String` and rounds it up to the given number of digits after decimal point.
+
+    x = 3.141592653589793
+
+    ceiling 2 x -- "3.15"
+    ceiling 4 x -- "3.1416"
+-}
 ceiling : Int -> Float -> String
 ceiling =
   roundFun Basics.ceiling
 
+{-| Turns a `Float` into a `String` and rounds it down to the given number of digits after decimal point.
+
+    x = 3.141592653589793
+
+    floor 2 x -- "3.14"
+    floor 4 x -- "3.1415"
+-}
 floor : Int -> Float -> String
 floor =
   roundFun Basics.floor
 
-{-| Away from zero
+{-| Turns a `Float` into a `String` and rounds it to the given number of digits after decimal point the commercial way.
+
+    x = -0.5
+
+    round 0 x -- "0"
+    roundCom 0 x -- "-1"
 -}
 roundCom : Int -> Float -> String
 roundCom =
@@ -190,6 +282,13 @@ roundCom =
             Basics.round fl
     )
 
+{-| Turns a `Float` into a `String` and rounds it down to the given number of digits after decimal point the commercial way.
+
+    x = -0.5
+
+    floor 0 x -- "-1"
+    floorCom 0 x -- "0"
+-}
 floorCom : Int -> Float -> String
 floorCom s fl =
   if fl < 0
@@ -198,6 +297,13 @@ floorCom s fl =
     else
       floor s fl
 
+{-| Turns a `Float` into a `String` and rounds it up to the given number of digits after decimal point the commercial way.
+
+    x = -0.5
+
+    ceiling 0 x -- "0"
+    ceilingCom 0 x -- "-1"
+-}
 ceilingCom : Int -> Float -> String
 ceilingCom s fl =
   if fl < 0
@@ -206,26 +312,38 @@ ceilingCom s fl =
     else
       ceiling s fl
 
+{-| As `round` but turns the resulting `String` back to a `Float`.
+-}
 roundNum : Int -> Float -> Float
 roundNum =
   funNum round
 
+{-| As `floor` but turns the resulting `String` back to a `Float`.
+-}
 floorNum : Int -> Float -> Float
 floorNum =
   funNum floor
 
+{-| As `ceiling` but turns the resulting `String` back to a `Float`.
+-}
 ceilingNum : Int -> Float -> Float
 ceilingNum =
   funNum ceiling
 
+{-| As `roundCom` but turns the resulting `String` back to a `Float`.
+-}
 roundNumCom : Int -> Float -> Float
 roundNumCom =
   funNum roundCom
 
+{-| As `floorCom` but turns the resulting `String` back to a `Float`.
+-}
 floorNumCom : Int -> Float -> Float
 floorNumCom =
   funNum floorCom
 
+{-| As `ceilingCom` but turns the resulting `String` back to a `Float`.
+-}
 ceilingNumCom : Int -> Float -> Float
 ceilingNumCom =
   funNum ceilingCom
